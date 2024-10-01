79072fcb69454107d16d3baa1c2f1e041b53b62f
Vitoriano <vitorianoernandes2012@gmail.com>
2024-10-01 18:05:35 -0300

    inital commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key = os.environ.get("OPENAI_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print(chat_completion)
